b9c8d56c0da532a2d296e3355f167f5912a1025d
Karo Launonen <karo.launonen@boogiesoftware.com>
2025-09-04 09:12:04 +0300

    Add keymap_generator build step, linker script, Zig 0.15.1 compatibility

--- build.zig
+++ build.zig
@@ -8,6 +8,28 @@ const MicroBuild = microzig.MicroBuild(.{
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
+    const keymap_generator = b.addExecutable(.{
+        .name = "keymap_generator",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/keymap_generator.zig"),
+            .target = b.graph.host,
+        }),
+    });
+
+    const run_keymap_generator = b.addRunArtifact(keymap_generator);
+    run_keymap_generator.addArgs(&.{
+        "src/keymap.zig",
+    });
+    run_keymap_generator.addFileInput(b.path("src/keymap.zig"));
+
+    const keymap_entry_path = run_keymap_generator.addOutputFileArg("zig-out/keymap_entry.zig");
+
+    const keymap_entry_module = b.createModule(.{
+        .root_source_file = keymap_entry_path,
+    });
+
+    const generator_step = b.step("keymap_generator", "Generates keymap_entry.zig file");
+
     const mz_dep = b.dependency("microzig", .{});
     const mb = MicroBuild.init(b, mz_dep) orelse return;
 
@@ -16,8 +38,14 @@ pub fn build(b: *std.Build) void {
         .target = mb.ports.rp2xxx.boards.raspberrypi.pico,
         .optimize = optimize,
         .root_source_file = b.path("src/main.zig"),
-        .linker_script = b.path("rp2040.ld"),
+        .linker_script = .{
+            .generate = .none,
+            .file = b.path("rp2040.ld"),
+        },
     });
+
+    firmware.artifact.step.dependOn(generator_step);
+    firmware.app_mod.addImport("keymap_entry", keymap_entry_module);
 
     const want_uf2 = b.option(bool, "uf2", "Build uf2 image") orelse false;
     if (want_uf2) {
